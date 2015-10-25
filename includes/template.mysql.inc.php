43cce75496e6586d99b026ef7db8fbee867dd391
Marvin Gölz <marvin.goelz@haw-hamburg.de>
2015-10-25 14:23:25 +0100

    Template für SQL Daten erstellt.

---
+++ includes/template.mysql.inc.php
@@ -0,0 +1,11 @@
+<? 
+	/*	Dies ist eine Vorlage zum Eintragen der sensiblen MySQL Daten.
+	 *  
+	 *  Achtung: niemals Passwörter ins Repository pushen. Diese Datei Lokal anpassen
+	 *  und nicht mit committen.
+	 */
+
+	define('DB_USERNAME',	'username');		//MySQL Benutzername
+	define('DB_PASSWORD',	'password');		//Password
+	define('DB_NAME',		'databasename');	//Name der Datenbank
+	define('DB_SERVER',		'databasehost'); 	//oft localhost
