c4f9329939b8d57a348c4d92aa6a1003b000d549
Marvin Gölz <marvin.goelz@haw-hamburg.de>
2015-10-26 12:22:27 +0100

    width/height des Canvas werden per JS gesetzt. New Game Button wird per JS mit ClockEvent 'startNewGame()' versehen.
    Startgrafik hinzugefügt / CSS: Canvas bekommt Rahmen, nur zur besseren sichtbarkeit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+// Temporäre Konstanten
+var CANVAS_WIDTH = 1280;
+var CANVAS_HEIGHT = 720;
+var COWLIFE_WELCOME_SCREEN_IMAGE_FILE= 'images/cowlife_screen.png';
+
+var CANVAS_ID = 'maincanvas';
+var START_NEW_GAME_BUTTON_ID = 'startNewGameButton';
+
+window.onload=function() {
+	var ctx = setCanvasSizeAndReturn2DContext(CANVAS_ID, CANVAS_WIDTH, CANVAS_HEIGHT);
+	bindStartNewGameButton(START_NEW_GAME_BUTTON_ID);
+}
+
+
+
+function setCanvasSizeAndReturn2DContext(id, width, height) {
+	canvasToInit = document.getElementById(id);
+	canvasToInit.setAttribute('width', width);
+	canvasToInit.setAttribute('height', height);
+
+	var contextToReturn = canvasToInit.getContext('2d');
+	return contextToReturn;
+}
+
+function bindStartNewGameButton(id) {
+	elementToBind = document.getElementById(id);
+	elementToBind.setAttribute('onclick', 'startNewGame()');
+}
+
+function startNewGame() {
+	alert("new game");
+}
