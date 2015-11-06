99e35eb057e31dd04c0b95f8627f91c060321229
Marvin Gölz <marvin.goelz@haw-hamburg.de>
2015-11-06 11:40:14 +0100

    Die drei MySQL Queries von Philipp eingefügt
    Beim Aufrufen dieser Datei wird eine SQL Verbindung hergestellt(dazu lokal angepasste Version von template.mysql.inc.php (Kopie davon in mysql.inc.php umbenennen) und die Queries werden ausgeführt. Bei Fehler werden die SQL Fehlermeldungen ausgegeben.
    
    Anmerkung: Queries funktionieren noch nicht, Syntaxfehler

--- createtables.php
+++ createtables.php
@@ -16,15 +16,78 @@
 		// Pro table erstmal einer dieser Abschnitte, wenns mehr wird macht es Sinn
 		// ein Query Array per Schleife zu durchlaufen.
 	    
-	    $query = '';
+	    $query = 'CREATE TABLE scenes
+		(
+		id SMALLINT,
+		name VARCHAR,
+		picturelink TEXT,
+		objectid SMALLINT,
+		object2id SMALLINT,
+		object3id SMALLINT,
+		object4id SMALLINT,
+		object5id SMALLINT,
+		position1x SMALLINT,
+		position2x SMALLINT,
+		position3x SMALLINT,
+		position4x SMALLINT,
+		position5x SMALLINT,
+		position1y SMALLINT,
+		position2y SMALLINT,
+		position3y SMALLINT,
+		position4y SMALLINT,
+		position5y SMALLINT,
+		UNIQUE(id, name),
+		PRIMARY KEY (id)
+		)';
 
 		if(mysqli_query($connection, $query)) {
-			echo ('Tabelle XY erzeugt.');
+			echo ('Tabelle erzeugt.');
 		}
 		else {
 			$isFailed = true;
-			echo ('Fehler beim Erzeugen der Tabelle XY: ') . mysqli_error($connection);	
+			echo ('Fehler beim Erzeugen der Tabelle: ') . mysqli_error($connection);	
 		}
+
+
+	    $query = "CREATE TABLE objects
+		(
+		id SMALLINT(size),
+		picturelink TEXT,
+		eventid SMALLINT(size),
+		name VARCHAR(size),
+		position SMALLINT,
+		UNIQUE(id, name),
+		PRIMARY KEY (id)
+		)";
+
+		if(mysqli_query($connection, $query)) {
+			echo ('<br>Tabelle erzeugt.');
+		}
+		else {
+			$isFailed = true;
+			echo ('<br><br>Fehler beim Erzeugen der Tabelle: ') . mysqli_error($connection);	
+		}
+
+
+	    $query = 'CREATE TABLE events
+		(
+		ID SMALLINT(size),
+		NAME VARCHAR(size),
+		CODE MEDIUMTEXT,
+		UNIQUE(id, name),
+		PRIMARY KEY (id)
+		)';
+
+		if(mysqli_query($connection, $query)) {
+			echo ('<br>Tabelle erzeugt.');
+		}
+		else {
+			$isFailed = true;
+			echo ('<br><br>Fehler beim Erzeugen der Tabelle: ') . mysqli_error($connection);	
+		}
+
+
+							
 
 		//
 
@@ -33,7 +96,7 @@
 			echo ('Alle Tabellen erfolgreich erzeugt');
 		}
 		else {
-			echo ('Fehler bei mindestens einer Tabelle');
+			echo ('<br><br>Fehler bei mindestens einer Tabelle');
 		}
 	}
 ?>
