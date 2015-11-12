e57f20eebece923538baca7895b5cb1a6ad7eef1
PhilippSteinke <phil.steinke@gmx.de>
2015-11-12 12:27:32 +0100

    Update createtables.php

--- createtables.php
+++ createtables.php
@@ -16,7 +16,27 @@
 		// Pro table erstmal einer dieser Abschnitte, wenns mehr wird macht es Sinn
 		// ein Query Array per Schleife zu durchlaufen.
 	    
-	    $query = ' ';		// HIER MYSQL QUERY EINFÜGEN
+	    $query = ' CREATE TABLE scenes
+(
+id INT UNIQUE PRIMARY KEY,
+name VARCHAR(30) NOT NULL UNIQUE,
+picturelink TEXT(240) NOT NULL,
+objectid INT,
+object2id INT,
+object3id INT,
+object4id INT,
+object5id INT,
+position1x INT,
+position2x INT,
+position3x INT,
+position4x INT,
+position5x INT,
+position1y INT,
+position2y INT,
+position3y INT,
+position4y INT,
+position5y INT
+)';	
 
 		if(mysqli_query($connection, $query)) {
 			echo ('Tabelle erzeugt.');
