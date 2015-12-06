5ab3c8f1eaa59751d9725b25f99b44195b9a9cf2
Marvin Gölz <marvin.goelz@haw-hamburg.de>
2015-12-06 15:40:36 +0100

    loadscene.php erstellt
    Dieses Script wird vom JavaScript per Ajax aufgerufen und liest die notwendigen Daten für die Generierung einer Scene aus der MySQL Datanbank aus und gibt diese Daten an das JS zurück

---
+++ loadscene.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+
+Dieses Script wird der Ajax Request vom JavaScript aufgerufen. Hier findet der Zugriff auf die MySQL Datenbank statt.
+Es werden die notwendigen Daten für eine Scene ausgelesen und die Daten werden zurück ans JS gegeben. 
+
+*/
+
+
+
+
+
+?>
