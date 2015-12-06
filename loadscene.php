89b424e34799c1a283831cddda80bc3a6056bdb3
Marvin Gölz <marvin.goelz@haw-hamburg.de>
2015-12-06 16:03:03 +0100

    loadscene vorbereitet

--- loadscene.php
+++ loadscene.php
@@ -7,8 +7,31 @@ Es werden die notwendigen Daten für eine Scene ausgelesen und die Daten werden 
 
 */
 
+if($_SERVER["REQUEST_METHOD"] == "POST" and isset($_POST['sceneid'])) {
+
+	require('includes/mysql.inc.php');
+	$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+	if(!$connection) {
+		die('MYSQL Connection failed: '.mysqli_connect_error());
+	}
 
 
+	//Ab hier sollen die Daten aus der SQL DB ausgelesen werden
+	$sceneName = 'Day of the Cow';
+	$sceneBackgroudPath = 'images/sc1_bg.png';
+	
+	$sceneObjectPositionX = array();
+	$sceneObjectPositionY = array();
 
+	$objectId = array();
+	$objectPicPath = array();
+	$objectName = array();
+	$objectEventId = array();
+
+
+	// Hier die Ausgabe ans JS:
+	echo('');
+}
 
 ?>
